9c9beff446e942307f1d7a1dac6facf044a9c22d
Victor Mendez <victor.ivan.mendez@gmail.com>
2022-12-17 23:57:53 -0600

    [TT0005-Classes-Refectorin] classes and method optimized

--- lib/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Kriptosio\Bowling\Lib\ScoreBoard;
-
-// Code goes here \/\/\
-$turn = new ScoreBoard('scores.txt');
-
-echo (string) $turn;
